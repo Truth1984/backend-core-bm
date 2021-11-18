894f232941d45b8d2f2aafbefa65851fb9649b85
truth1984 <zhengyao365@163.com>
2021-11-18 10:13:09 +0800

    fix path normalize

--- core.js
+++ core.js
@@ -62,7 +62,7 @@ un.textEncryptBase64 = (text, encrypting = true) => {
  * normalize path, also replace ~ with $home
  */
 un.filePathNormalize = (...path) =>
-  u.stringReplace(paths.normalize(paths.join(...path)), { "~": process.env.HOME, "\\\\\\\\": "/", "\\\\": "/" });
+  u.stringReplace(paths.normalize(paths.join(...path)), { "^~": process.env.HOME, "\\\\\\\\": "/", "\\\\": "/" });
 
 un.filePathFull = (...path) => paths.resolve(...path);
 
